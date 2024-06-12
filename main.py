80f1439c65741de65de0ed5c30f2e1040d344680
Eli Blazku <eli.blazek20@gmail.com>
2024-06-12 16:38:43 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-def main(book):
-    with open(book) as f:
+def get_open_book(path):
+    with open(path) as f:
         open_book = f.read()
-        words = open_book.split()
-        print(len(words))
+    return open_book
 
-main("books/frankenstein.txt")
+def get_count_words(res_string):
+    words = res_string.split()
+    print(len(words))
+
+def main():
+    book = get_open_book("books/frankenstein.txt")
+    get_count_words(book)
+
+main()
+
+
+
