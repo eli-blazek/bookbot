d3db477d6c87d834c78817fccfeb8d8bca096a01
Eli Blazku <eli.blazek20@gmail.com>
2024-07-22 19:54:44 +0200

    Some more refactoring

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def main():
-    path = "books/frankenstein.txt"
-    text = read_book(path)
-    print(count_words(text))
-
 def read_book(book_path):
     with open(book_path) as raw_data:
         book_contents = raw_data.read()
@@ -12,7 +7,13 @@ def count_words(text_source):
     words = text_source.split()
     return len(words)
 
-def count_chars():
+def count_chars(text_source):
     pass
+
+def main():
+    path = "books/frankenstein.txt"
+    text = read_book(path)
+    count_chars(text)
+    print(count_words(text))
 
 main()
